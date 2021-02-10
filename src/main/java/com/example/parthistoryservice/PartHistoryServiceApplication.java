36c0b0d0a329947eef4bd11511907f2fb0e5091c
debraj1976 <De365600@@>
2021-02-09 22:08:57 -0600

    JWT authentication inbuild

--- src/main/java/com/example/parthistoryservice/PartHistoryServiceApplication.java
+++ src/main/java/com/example/parthistoryservice/PartHistoryServiceApplication.java
@@ -3,11 +3,25 @@ package com.example.parthistoryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableSwagger2
+//@EntityScan(basePackageClasses = {
+//		PartHistoryServiceApplication.class,
+//		Jsr310JpaConverters.class
+//})
 public class PartHistoryServiceApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PartHistoryServiceApplication.class, args);
---
+++ src/main/java/com/example/parthistoryservice/models/RoleName.java
@@ -0,0 +1,9 @@
+package com.example.parthistoryservice.models;
+
+/**
+ * Created by rajeevkumarsingh on 07/12/17.
+ */
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
